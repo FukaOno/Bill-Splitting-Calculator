ae264708ca4b3f538ea7146f5a8fea5294d98754
FukaOno <atopfuka@gmail.com>
2024-08-09 22:00:10 +0900

    Update main.js

--- main.js
+++ main.js
@@ -10,16 +10,16 @@ function calculate() {
     const resultElement = document.getElementById("result");
 
 
-resultElement.innerHTML="1人あたり：";
+resultElement.innerHTML="1人あたり： / Per person";
 
 if(isNaN(amount) || isNaN(people)) {
-    resultElement.innerHTML += '<span class="error">金額と人数の両方を入力してください。</span>';    
+    resultElement.innerHTML += '<span class="error">金額と人数の両方を入力してください。/ Please enter both the price and number of people.</span>';    
     return;
 
 }
 
 if(amount < 1 || people < 1){
-    resultElement.innerHTML += '<span class="error">1以上の数値を入力してください。</span>';
+    resultElement.innerHTML += '<span class="error">1以上の数値を入力してください。/ Please enter the number bigger than 1.</span>';
     return;
     
 }
@@ -30,10 +30,10 @@ const roundedResult = Math.floor(result);
 const remainder = amount - (roundedResult * people);
                       
 if (remainder === 0) {
-    resultElement.textContent = `1人あたり：${roundedResult}円`;
+    resultElement.textContent = `1人あたり：/ Per person${roundedResult}円`;
 } else {
     const shortage = people - remainder;
-    resultElement.textContent = `1人あたり：${roundedResult}円で${remainder}円不足します。1人あたり${roundedResult + 1}円にすると${shortage}円余ります。`;
+    resultElement.textContent = `1人あたり：/ Per person${roundedResult}円で/ yen ${shortage}円不足します。/ yen lacks.1人あたり/ If per person${roundedResult + 1}円にすると / yen then,${remainder}円余ります。/ yen remains`;
 }
 }
 
